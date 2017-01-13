829afbf190d0d0f1c4129406a1f1d5cb7f46178d
Kamil <kpiotrowski@comea.pl>
2017-01-13 23:32:05 +0100

    automatyczne dostosowywanie wielkości stosu

--- games-src/Players/KMPlayer/src/main/java/put/ai/games/kmplayer/KMPlayer.java
+++ games-src/Players/KMPlayer/src/main/java/put/ai/games/kmplayer/KMPlayer.java
@@ -10,12 +10,14 @@ import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
+import static java.lang.Math.cbrt;
 import static java.lang.Math.sqrt;
 
 public class KMPlayer extends Player {
 
-    private Random random=new Random(0xdeadbeef);
     private long startTime;
+    private int maxDepth=1;
+    private float maxValue;
 
     /**
      * Funcka oceny stanu
@@ -23,10 +25,10 @@ public class KMPlayer extends Player {
      * @return Wartość float im większa tym lepsza nasza sutuacja
      */
     private Float evaluateBoard(Board board){
-        Color opponent = this.getOpponent(getColor());
+        Color opponent = getOpponent(getColor());
         Integer opponentMoves = board.getMovesFor(opponent).size();
         Integer myMoves = board.getMovesFor(getColor()).size();
-        if(opponentMoves==0) return (float) (board.getSize() * board.getSize());
+        if(opponentMoves==0) return this.maxValue;
         return (float) (myMoves) / opponentMoves;
     }
 
@@ -38,7 +40,13 @@ public class KMPlayer extends Player {
             board.doMove(m);
             Float value;
             if(depth==0) value = evaluateBoard(board);
-            else value = checkMove(board,depth,false);
+            else {
+                value = checkMove(board,depth,false);
+                if(value==null) {
+                    depth=0;
+                    value = evaluateBoard(board);
+                }
+            }
             if(value>=max){
                 max = value;
                 selectedMove=m;
@@ -46,24 +54,36 @@ public class KMPlayer extends Player {
         }
         return selectedMove;
     }
-    private float checkMove(Board b, int depth, boolean myMove){
+    private Float checkMove(Board b, int depth, boolean myMove){
         depth--;
-        if(depth>0 && System.currentTimeMillis()-startTime > getTime()/2) depth = 0;
+        if(depth>0 && System.currentTimeMillis()-startTime > sqrt(getTime())) {
+            this.maxDepth--;
+            return null;
+        }
         List<Move> moves;
         if(!myMove) moves = b.getMovesFor(getOpponent(this.getColor()));
         else moves = b.getMovesFor(this.getColor());
-        if(moves.size()==0 && myMove) return 0;
-        if(moves.size()==0 && !myMove) return (float) b.getSize()*b.getSize();
+        if(moves.size()==0 && myMove) return 0f;
+        if(moves.size()==0 && !myMove) return this.maxValue;
 
         Float returnValue=0f;
-        if(!myMove) returnValue= (float) b.getSize()*b.getSize();
+        if(!myMove) returnValue = this.maxValue;
         for(Move m : moves){
             Board board = b.clone();
             board.doMove(m);
             Float value = 0f;
             if(depth==0) value = evaluateBoard(board);
-            else value = checkMove(board, depth, !myMove);
-            if(myMove && value>=returnValue) returnValue=value;
+            else {
+                value = checkMove(board, depth, !myMove);
+                if(value==null) {
+                    depth=0;
+                    value = evaluateBoard(board);
+                }
+            }
+            if(myMove && value>=returnValue) {
+                if(value == this.maxValue) return this.maxValue;
+                returnValue=value;
+            }
             if(!myMove && value<=returnValue) returnValue=value;
         }
         return returnValue;
@@ -76,9 +96,11 @@ public class KMPlayer extends Player {
 
     @Override
     public Move nextMove(Board b) {
+        this.maxValue = b.getSize()*b.getSize();
         this.startTime = System.currentTimeMillis();
-        int depth=3;
-        return selectMove(b, depth);
+        Move move = selectMove(b, this.maxDepth);
+        if( (double)(System.currentTimeMillis()-startTime) < sqrt(getTime())) this.maxDepth++;
+        return move;
     }
 
     public static void main(String args[]){}
